67079c3f3f593e456580c74843cf17bfd4081032
aashish-rupreja-mikado <aashish.rupreja.mikado@gmail.com>
2025-10-21 14:56:16 +0530

    Add mapping for events page

---
+++ scripts/spa-engine/spa-link-to-page-map.js
@@ -0,0 +1,6 @@
+const linkToPageMap = new Map([
+    ["explore-art", {"page": "homepage", "title": "Virtual Art Gallery"}],
+    ["upcoming-events", {"page": "Events", "title": "Upcoming Events | Virtual Art Gallery"}]
+]);
+
+export { linkToPageMap };
