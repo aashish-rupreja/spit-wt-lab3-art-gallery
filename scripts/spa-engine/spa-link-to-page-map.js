437bdccabc2b5bde7e363be58bbb18534ce73793
aashish-rupreja-mikado <aashish.rupreja.mikado@gmail.com>
2025-10-23 03:48:30 +0530

    Add mapping for all pages

--- scripts/spa-engine/spa-link-to-page-map.js
+++ scripts/spa-engine/spa-link-to-page-map.js
@@ -1,6 +1,13 @@
 const linkToPageMap = new Map([
-    ["explore-art", {"page": "homepage", "title": "Virtual Art Gallery"}],
-    ["upcoming-events", {"page": "Events", "title": "Upcoming Events | Virtual Art Gallery"}]
+    ["sign-up", {"page": "sign-up", "title": "Sign Up"}],
+    ["sign-in", {"page": "sign-in", "title": "Sign In"}],
+    ["homepage", {"page": "homepage", "title": "Kalakriti"}],
+    ["explore-art", {"page": "homepage", "title": "Explore Art"}],
+    ["categories", {"page": "categories", "title": "Categories"}],
+    ["favorites", {"page": "favorites", "title": "Favorites"}],
+    ["about-us", {"page": "about-us", "title": "About Us"}],
+    ["contact-us", {"page": "contact-us", "title": "Contact Us"}],
+    ["upcoming-events", {"page": "events", "title": "Upcoming Events"}]
 ]);
 
 export { linkToPageMap };
