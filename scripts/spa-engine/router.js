672c6f99352a46f771d087d28b7437a5596f91d2
aashish-rupreja-mikado <aashish.rupreja.mikado@gmail.com>
2025-10-25 17:04:32 +0530

    Ensure required scripts load on each page

--- scripts/spa-engine/router.js
+++ scripts/spa-engine/router.js
@@ -3,12 +3,21 @@ import { routes } from "./routes.js";
 const updateContainer = document.getElementById("update-container");
 const pagePathPrefix = "pages/";
 const stylePathPrefix = "styles/";
+const scriptPathPrefix = "scripts/";
 
 async function loadPage(path) {
     const pageName = routes[path] || `${pagePathPrefix}404`;
     const pageContent = await fetch(`${pagePathPrefix}${pageName}.html`);
     const pageHtml = await pageContent.text();
 
+    applyStyles(pageName);
+    applyScripts(pageName);
+
+    // path != "/" ? document.title = document.querySelector(`a[href="${path}"]`).innerText : '';
+    updateContainer.innerHTML = pageHtml;
+}
+
+function applyStyles(pageName) {
     const linkElements = document.getElementsByTagName('link');
     let applyStyle = true;
     for (const linkElement of linkElements) {
@@ -23,9 +32,22 @@ async function loadPage(path) {
         styleLink.href = `${stylePathPrefix}${pageName}.css`;
         document.head.appendChild(styleLink);
     }
+}
 
-    // path != "/" ? document.title = document.querySelector(`a[href="${path}"]`).innerText : '';
-    updateContainer.innerHTML = pageHtml;
+function applyScripts(pageName) {
+    const scriptElements = document.getElementsByTagName('script');
+    let applyScript = true;
+    for (const scriptElement of scriptElements) {
+        if (scriptElement.src.includes(pageName)) {
+            applyScript = false;
+            break;
+        }
+    }
+    if (applyScript) {
+        const script = document.createElement('script');
+        script.src = `${scriptPathPrefix}${pageName}.js`;
+        document.body.appendChild(script);
+    }
 }
 
 export function handleRoute() {
