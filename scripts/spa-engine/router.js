8eef1ed4f19f4283fcaacf8cc479a7d9f58ba7f1
aashish-rupreja-mikado <aashish.rupreja.mikado@gmail.com>
2025-10-25 17:54:52 +0530

    Handle query parameters

--- scripts/spa-engine/router.js
+++ scripts/spa-engine/router.js
@@ -5,13 +5,16 @@ const pagePathPrefix = "pages/";
 const stylePathPrefix = "styles/";
 const scriptPathPrefix = "scripts/";
 
-async function loadPage(path) {
+async function loadPage(path, query = "") {
     const pageName = routes[path] || `${pagePathPrefix}404`;
     const pageContent = await fetch(`${pagePathPrefix}${pageName}.html`);
     const pageHtml = await pageContent.text();
 
     applyStyles(pageName);
     applyScripts(pageName);
+
+    const urlParams = new URLSearchParams(query);
+    window.currentPageParams = Object.fromEntries(urlParams.entries());
 
     // path != "/" ? document.title = document.querySelector(`a[href="${path}"]`).innerText : '';
     updateContainer.innerHTML = pageHtml;
@@ -52,8 +55,10 @@ function applyScripts(pageName) {
 
 export function handleRoute() {
     const path = window.location.pathname;
-    loadPage(path);
+    const query = window.location.search;
+    loadPage(path, query);
 }
+
 
 export function navigateTo(url, state = {}) {
     history.pushState(state, "", url);
