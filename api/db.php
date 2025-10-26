af93f500a39615e835b83428bcde2131e070d485
aashish-rupreja-mikado <aashish.rupreja.mikado@gmail.com>
2025-10-26 13:22:59 +0530

    Use SQLITE for ease of deployment and simplicity

--- api/db.php
+++ api/db.php
@@ -1,13 +1,11 @@
 <?php
-$dsn = "mysql:host=localhost;port=3306;dbname=LAB3;charset=utf8mb4";
-$user = "root";
-$pass = "Hell0!World#";
+$dbFile = __DIR__ . '/../db/LAB3.db';
 
 try {
-    $pdo = new PDO($dsn, $user, $pass, [
-        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
-    ]);
+    $pdo = new PDO("sqlite:" . $dbFile);
+
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 } catch (PDOException $e) {
     http_response_code(500);
     echo json_encode(["error" => $e->getMessage()]);
